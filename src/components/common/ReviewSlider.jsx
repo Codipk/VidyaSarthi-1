81f208469821022632639247459b20f6f764a22d
Sundram Mishra <97393268+sundram2003@users.noreply.github.com>
2023-07-06 13:28:27 +0530

    Update ReviewSlider.jsx

--- src/components/common/ReviewSlider.jsx
+++ src/components/common/ReviewSlider.jsx
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-import { Swiper, SwiperSlide } from "swiper/react"
-import "swiper/css"
-import "swiper/css/free-mode"
-import "swiper/css/pagination"
-import { Autoplay, FreeMode, Navigation, Pagination } from 'swiper'
-import ReactStars from "react-rating-stars-component"
-import { apiConnector } from '../../services/apiconnector'
-import { ratingsEndpoints } from '../../services/apis'
-import { FaStar } from 'react-icons/fa'
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import { Autoplay, FreeMode, Navigation, Pagination } from "swiper";
+import ReactStars from "react-rating-stars-component";
+import { apiConnector } from "../../services/apiconnector";
+import { ratingsEndpoints } from "../../services/apis";
+import { FaStar } from "react-icons/fa";
 
 const ReviewSlider = () => {
-
   const [reviews, setReviews] = useState([]);
   const truncateWords = 15;
 
-
   useEffect(() => {
     const fetchAllReviews = async () => {
-      const { data } = await apiConnector("GET", ratingsEndpoints.REVIEWS_DETAILS_API)
+      const { data } = await apiConnector(
+        "GET",
+        ratingsEndpoints.REVIEWS_DETAILS_API
+      );
       console.log("LOgging response in rating", data);
 
       if (data?.success) {
@@ -27,15 +27,13 @@ const ReviewSlider = () => {
       }
 
       console.log("Printing Reviews", reviews);
-
-    }
+    };
     fetchAllReviews();
   }, []);
 
-
   return (
-    <div className='text-white'>
-      <div className='h-[190px] max-w-maxContent'>
+    <div className="text-white  py-3">
+      <div className="h-[300px] max-w-maxContent py-[3px]">
         <Swiper
           slidesPerView={4}
           spaceBetween={24}
@@ -44,26 +42,40 @@ const ReviewSlider = () => {
           autoplay={{
             delay: 2500,
           }}
+          pagination={{ clickable: true }}
           modules={[FreeMode, Pagination, Autoplay]}
-          className='w-full'
+          // className="w-full"
         >
-
-          {
-            reviews.map((review, index) => (
-              <SwiperSlide key={index}>
+          {reviews.map((review, index) => (
+            <SwiperSlide
+              key={index}
+              className="max-w-sm h-[250px] p-2 bg-richblack-800 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-richblack-800 "
+            >
+              <div className="flex flex-row gap-3 ">
                 <img
-                  src={review?.user?.image
-                    ? review?.user?.image
-                    : `https://api.dicebear.com/5.x/initials/svg?seed=${review?.user?.firstName} ${review?.user?.lastName}`}
-                  alt='Profile Pic'
-                  className='h-9 w-9 object-cover rounded-full'
+                  src={
+                    review?.user?.image
+                      ? review?.user?.image
+                      : `https://api.dicebear.com/5.x/initials/svg?seed=${review?.user?.firstName} ${review?.user?.lastName}`
+                  }
+                  alt="Profile Pic"
+                  className="h-9 w-9 object-cover rounded-full"
                 />
-                <p>{review?.user?.firstName} {review?.user?.lastName}</p>
-                <p>{review?.course?.courseName}</p>
+
                 <p>
-                  {review?.review}
+                  {review?.user?.firstName} {review?.user?.lastName}
                 </p>
-                <p>{review?.rating.toFixed(1)}</p>
+              </div>
+              <div className="m-2"></div>
+              <p className="  shadow-white text-richblack-50 font-semibold ">
+                {review?.course?.courseName}
+                <div className="border-dashed  h-[0.5px] bg-richblack-100"></div>
+              </p>
+              <p className=" overflow-hidden ">{review?.review}</p>
+              <div className="flex flex-row gap-2    ">
+                <p className="mt-[4px]  items-baseline ">
+                  {review?.rating.toFixed(1)}
+                </p>
                 <ReactStars
                   count={5}
                   value={review.rating}
@@ -72,15 +84,15 @@ const ReviewSlider = () => {
                   activeColor="#ffd700"
                   emptyIcon={<FaStar />}
                   fullIcon={<FaStar />}
+                  required={true}
                 />
-              </SwiperSlide>
-            ))
-          }
-
+              </div>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ReviewSlider
+export default ReviewSlider;
